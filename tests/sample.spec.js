69e236ed09a74a8387c742e026d354e0079b7238
VikneshCoder <vikneshwaraj1302@gmail.com>
2025-05-03 05:17:47 -0700

    Login into the application

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,6 +1,22 @@
 const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://www.youtube.com/watch?v=__vH5uFaZOE');
-  const title = await page.title();
+  await page.goto('https://rahulshettyacademy.com/client');
+  await page.waitForLoadState('networkidle');
+  const title = await page.title()
+  console.log(title); // Log the title to the console
+  expect(title).toBe("Let's Shop");   // Verify the title of the page
+
+  // Login to the application
+  await page.locator("//input[@id='userEmail']").fill('vikneshraj1302@gmail.com');
+  await page.locator("//input[@id='userPassword']").fill('Test@123');
+  await page.locator("//input[@id='login']").click();
+
+  // Verify that we are in the home page
+  await page.waitForLoadState('networkidle');
+  const home = await page.locator("//section[@id='sidebar']//p[1]").textContent();
+  console.log(home); // Log the home text to the console
+  expect(home).toContain("Home"); // Verify that the home text contains "Home"
+
 });
+
